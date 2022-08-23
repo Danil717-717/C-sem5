6cd9dd3ddeb9ca4541bfe0f522bf3c660e54efdf
Danil <danila_monster@mail,ru>
2022-08-23 16:54:17 +0300

    ДЗ_006

---
+++ Task006/Program.cs
@@ -0,0 +1,38 @@
+﻿double [] CreateArray(int length, int min, int max)     
+{
+    double[] array = new double[length];
+    for(int i = 0; i < length; i++)                  
+    {
+        array[i] = new Random().NextDouble()*(min - max) + min;
+        array[i] = Math.Round(array[i], 2);  
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    for(int i = 0; i < array.Length; i++ ) 
+        Console.Write($"{array[i]} ");
+
+    Console.WriteLine();               
+}
+
+double MinMax(double[] array)
+{                                                  
+    double maxNumber = array[0];
+    double minNumber = array[0];
+
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (maxNumber < array[i]) maxNumber = array[i];
+        if (minNumber > array[i]) minNumber = array[i];
+    }
+    double dif = maxNumber - minNumber;
+    return dif;
+}
+
+double [] array = CreateArray(10, 1, 100);
+PrintArray(array);
+Console.WriteLine();
+double difference = MinMax(array);
+Console.WriteLine($"sdf {difference} ");
